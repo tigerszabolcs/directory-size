92cc9f9e61091a4ba867f029c7eac3af528f5f3f
tigerszabolcs <tigerszabolcs@gmail.com>
2022-03-02 16:13:17 +0100

    cleared up some stuff

--- filesize.py
+++ filesize.py
@@ -7,6 +7,13 @@ class bigFiles:
         self.fSize = fSize
         pass
 
+class dirSizes:
+    def __init__(self, dirPath, dirSize, lFile) -> None:
+        self.dirPath = dirPath
+        self.dirSize = dirSize
+        self.lFile = lFile
+        pass
+
 def ConvertSize(size,measure):
     if measure == "MB":
         return size / (1024*1024)
@@ -15,30 +22,22 @@ def ConvertSize(size,measure):
     else:
         return size
 
+
+
 def calculateSizeOfDir(rootDir):
-    rootdir = 'C:/Games/'
-    filesize = 0
-    maxsize = 0
-    maxfile = ""
-    history = []
+    rootdir = rootDir
+
     totalSize = 0
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             fpath = os.path.join(subdir, file)
-            filesize = os.path.getsize(fpath)
             if not os.path.islink(fpath):
                 totalSize += os.path.getsize(fpath)
-            #if (filesize > maxsize) and (filesize > 10000000):
-            #   maxsize = filesize            
-            #  maxfile = fpath
-            # n = bigFiles(maxfile,maxsize)
-            # history.append(n)
-            # print(maxfile,maxfile, sep=' ')
-            # continue
-            #print(os.path.join(subdir, file))
             continue
     # print("Legnagyobb file: ", maxfile, " " , "mérete: " , maxsize )
     # for x in history:
     #    print(x.fSize, x.fName)
     print("size in MB: ", int(ConvertSize(totalSize,"MB")), "|| size in GB: ", int(ConvertSize(totalSize,"GB")), sep=' ') 
     return totalSize()
+
+
